53bee4bd7aba143768c657365bbcecd441a266e3
Léo.B <leo.berteloot@gmail.com>
2022-12-29 13:23:18 +0100

    Add files via upload

---
+++ theme_config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+You can change the color with some color like:
+- default
+- red
+- yellow
+- blue
+- green
+
+If you want to add more color, copy the default CSS file in css/color/ to your new color and add this here
+*/
+$themecolor = 'default';
